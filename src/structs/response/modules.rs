2b1ea6efa5ea3052a7a028f55fc6aefd96f7a70f
Artur A <artur.aralin97@gmail.com>
2023-02-23 23:48:56 +0300

    fix schema

--- src/structs/response/modules.rs
+++ src/structs/response/modules.rs
@@ -43,7 +43,7 @@ pub struct encode {
 #[derive(Deserialize, Debug)]
 pub struct execute_non_payloads {
   pub job_id: i32,
-  pub uuid: uuid::Uuid,
+  // pub uuid: uuid::Uuid,
 }
 #[derive(Deserialize, Debug)]
 pub struct execute_payloads {
